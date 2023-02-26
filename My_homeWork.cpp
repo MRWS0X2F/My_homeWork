1f1f09c3f3e933067527165519ae50a6924b9214
chen-lao-wan <3225056883@qq.com>
2023-02-26 21:08:52 +0800

    添加项目文件。

---
+++ My_homeWork.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	
+	for (int num = 1000;num < 10000;num++)
+	{
+		a = num % 10;
+		b = num / 10 % 10;
+		c = num / 100 % 10;
+		d = num / 1000;
+		if (d + b == 8 && c + a == 8 && d < c && num % 7 == 3)
+		{
+			cout << num << endl;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
