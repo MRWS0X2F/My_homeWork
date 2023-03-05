6af57e5808ebfcec24a65b0121d6d3ec8a90336c
chen-lao-wan <3225056883@qq.com>
2023-03-05 21:00:00 +0800

    项目文件

--- My_homeWork.cpp
+++ My_homeWork.cpp
@@ -11,10 +11,22 @@ int main()
 		b = num / 10 % 10;
 		c = num / 100 % 10;
 		d = num / 1000;
-		if (d + b == 8 && c + a == 8 && d < c && num % 7 == 3)
-		{
-			cout << num << endl;
+		if (a != 0 && b != 0 && c != 0 && d != 0){
+			if (a != b && a != c && a != d && b != c && b != d && c != d) {
+				if (d - a == b + c && (num + 1000 * b + 100 * a + 10 * d + c) % 101 == 0)
+				{
+					cout << num << "\t" << (num + 1000 * b + 100 * a + 10 * d + c) / 101 << endl;
+				}
+			}
 		}
+		/*
+		* 9153    144
+		* 9351    144
+		*/
+		//if (d + b == 8 && c + a == 8 && d < c && num % 7 == 3)
+		//{
+		//	cout << num << endl;
+		//}
 	}
 
 	system("pause");
